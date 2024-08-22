d29b22c5ed66a8c48195084e66e32a1ead1df587
Aissam-salman <nasawi.lmj@gmail.com>
2024-08-22 14:32:35 +0200

    fix: deploy

--- components/threeName/user-layers.ts
+++ components/threeName/user-layers.ts
@@ -1,20 +1,20 @@
-import { useEffect, useRef, RefObject } from 'react'
+import {RefObject, useEffect, useRef} from 'react'
 import * as THREE from 'three'
 
 function useLayers<T extends THREE.Object3D>(layers: number[] = [0]): RefObject<T> {
-  const ref = useRef<T>(null!)
+    const ref = useRef<T>(null!)
 
-  useEffect(() => {
-    if (ref.current) {
-      ref.current.layers.disableAll()
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.layers.disableAll()
 
-      layers.sort().forEach((layer) => {
-        ref.current.layers.enable(layer)
-      })
-    }
-  }, [layers])
+            layers.sort().forEach((layer) => {
+                ref.current.layers.enable(layer)
+            })
+        }
+    }, [layers])
 
-  return ref
+    return ref
 }
 
 export default useLayers
